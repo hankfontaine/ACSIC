0e2b16097031a7d6fcb93e2e8232457b0f725754
HankFontaine <henrymcgill@gmail.com>
2022-07-05 12:33:06 -0400

    finished working version

---
+++ script.js
@@ -0,0 +1,6 @@
+// const userName = prompt("What's your name?").toUpperCase();
+
+// if (userName) {
+//   document.getElementById("userGreet").innerHTML = `@${userName}`;
+//   document.getElementById("userGreetTwo").innerHTML = `@${userName}`;
+// }
