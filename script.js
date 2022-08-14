c9c55a9e0d185a3f1235299288553153bbe6e597
HankFontaine <henrymcgill@gmail.com>
2022-08-14 10:31:38 -0700

    started rewriting project in js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-// const userName = prompt("What's your name?").toUpperCase();
+const firstProject = document.getElementById("project-one");
 
-// if (userName) {
-//   document.getElementById("userGreet").innerHTML = `@${userName}`;
-//   document.getElementById("userGreetTwo").innerHTML = `@${userName}`;
-// }
+const firstProjectTitle = document.createElement("div");
+firstProjectTitle.classList = "project-title";
+firstProjectTitle.innerHTML = "Super Cool Project";
+firstProject.appendChild(firstProjectTitle);
+const firstProjectText = document.createElement("div");
+firstProjectText.classList = "project-text";
+firstProjectText.innerHTML =
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse nisi rem minima expedita, facilis ex?";
+firstProject.appendChild(firstProjectText);
+const firstProjectLinks = document.createElement("div");
+firstProjectLinks.classList = "project-links";
+firstProject.appendChild(firstProjectLinks);
+const firstProjectStar = document.createElement("div");
+firstProjectStar.innerHTML = "star";
+firstProjectLinks.appendChild(firstProjectStar);
+const firstProjectEye = document.createElement("div");
+firstProjectEye.innerHTML = "eye";
+firstProjectLinks.appendChild(firstProjectEye);
+const firstProjectShare = document.createElement("div");
+firstProjectShare.innerHTML = "share";
+firstProjectLinks.appendChild(firstProjectShare);
